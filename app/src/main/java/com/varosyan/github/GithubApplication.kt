b680c432901336222031f78756c36214fee8ddac
davidvarosyan <davidvarosyan95@gmail.com>
2025-07-21 20:55:05 +0400

    create resources for hardcoded parameters

--- app/src/main/java/com/varosyan/github/GithubApplication.kt
+++ app/src/main/java/com/varosyan/github/GithubApplication.kt
@@ -1,14 +1,31 @@
 package com.varosyan.github
 
 import android.app.Application
+import android.util.Log
 import com.varosyan.connecter.getAppModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
 
+
 class GithubApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        val defaultUEH = Thread.getDefaultUncaughtExceptionHandler()
+
+
+        // set your own handler
+        Thread.setDefaultUncaughtExceptionHandler { thread, e -> // 1) Log/report the exception (e.g., to Crashlytics, your server, or local file)
+            Log.e("MyApp", "Uncaught exception in thread " + thread.name, e)
+
+            // 2) (Optional) Show your own error UI or restart logic
+            //    e.g. startActivity(new Intent(getApplicationContext(), ErrorActivity.class)
+            //             .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+
+            // 3) Delegate to default handler to kill the process
+            defaultUEH?.uncaughtException(thread, e)
+        }
         startKoin {
             androidContext(this@GithubApplication)
             loadKoinModules(getAppModules())
--- presenter/src/main/java/com/varosyan/presenter/user_detail/UserDetailFragment.kt
+++ presenter/src/main/java/com/varosyan/presenter/user_detail/UserDetailFragment.kt
@@ -41,21 +41,21 @@ class UserDetailFragment : Fragment(R.layout.fragment_user_detail) {
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.userDetail.collect { detail ->
                 viewBinding.apply {
-                    locationTextView.text = detail.location ?: "No Info"
+                    locationTextView.text = detail.location ?: NO_INFO
                     followersTextView.text = detail.followersCount.toString()
                     followingTextView.text = detail.followingCount.toString()
-                    bioTextView.text = detail.bio ?: "No Info"
+                    bioTextView.text = detail.bio ?: NO_INFO
                     nameTextView.text = detail.fullName ?: detail.userName
                     reposCountTextView.text = detail.repoCount.toString()
                     gistsCountTextView.text = detail.gistCount.toString()
                     detail.lastUpdated?.let {
                         val instant = Instant.parse(it)
                         val formatter = DateTimeFormatter
-                            .ofPattern("dd/MM/yyyy")
+                            .ofPattern(DATE_FORMAT)
                             .withZone(ZoneId.systemDefault())
                         formatter.format(instant)
                         updatedTextView.text = formatter.format(instant)
-                    } ?: run { updatedTextView.text = "No Info" }
+                    } ?: run { updatedTextView.text = NO_INFO }
                     Glide.with(avatarImageView)
                         .load(detail.avatar)
                         .placeholder(R.drawable.account_circle)
@@ -65,5 +65,10 @@ class UserDetailFragment : Fragment(R.layout.fragment_user_detail) {
                 }
             }
         }
+    }
+
+    companion object {
+        private const val NO_INFO = "No Info"
+        private const val DATE_FORMAT = "dd/MM/yyyy"
     }
 }
